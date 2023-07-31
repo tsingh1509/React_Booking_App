f97ba60403f59afe54ff1e2427f1662672498538
tsingh1509 <103776449+tsingh1509@users.noreply.github.com>
2023-07-31 17:39:57 +0530

    Add files via upload

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./featured.css"
+
+const Featured = () => {
+    return(
+        <div className="featured">
+            <div className="featuredItem">
+            <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                alt=""
+                className="featuredImg"
+            />
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+            </div>
+
+            <div className="featuredItem">
+            <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                alt=""
+                className="featuredImg"
+            />
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+            </div>
+
+            <div className="featuredItem">
+            <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                alt=""
+                className="featuredImg"
+            />
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Featured
